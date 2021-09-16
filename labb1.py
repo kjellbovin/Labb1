d5d3e27f1d8b8962a36ef2b1838114b2dd525ef7
Kjell Bovin <90247378+kjellbovin@users.noreply.github.com>
2021-09-16 11:21:52 +0200

    Added code in labb1

--- labb1.py
+++ labb1.py
@@ -1,9 +1,12 @@
 import math
-print('Här kan du räkna ut volymen för en liksidig kub samt en liksidig tetrahedron: ')
-var1 = (input("Ange längden i centimeter för en av figurens sidor: "))
+print('\n ****************************************************************************')
+print(' Här kan du räkna ut volymen för en liksidig kub och en liksidig tetrahedron')
+print(' ****************************************************************************')
+
+var1 = (input("Ange längden på en av figurens sidor (i cm): "))
 var1 = float(var1.replace(',', '.'))
 kub = round((var1 * var1 * var1), 2)
-sqrtroot = math.sqrt(12)
-tetra =round((kub /6), 2)
-print('Om sidan på en liksidig kub är ', var1, 'cm. ', 'Så är volymen', kub, 'cm3')
-print('Om sidan på en liksidig tetraeder är ', var1, 'cm.', 'Så är volymen')
+tetra = round(math.sqrt(kub /6), 2)
+
+print('Kubens volym', kub, 'cm3')
+print('Tetraeders volym', tetra, 'cm3')
