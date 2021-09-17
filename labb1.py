faa7ccecb7402ecadc9d785bd0e044e92bc733bd
Kjell Bovin <90247378+kjellbovin@users.noreply.github.com>
2021-09-17 11:05:39 +0200

    Changes in labb1.py

--- labb1.py
+++ labb1.py
@@ -7,7 +7,7 @@ print(' ************************************************************************
 var1 = (input("Ange längden på en av figurens sidor (i cm): "))
 var1 = float(var1.replace(',', '.'))
 volumeCube = var1 ** 3
-volumeTetra = var1 ** 3/6*math.sqrt(2)
+volumeTetra = volumeCube/6 * math.sqrt(2)
 
 print('Kubens volym', volumeCube, 'cm3')
 print('Tetraeders volym', volumeTetra, 'cm3')
