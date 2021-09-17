dfb9edbdc705ea3676093565e68e7af5cfee62e2
Kjell Bovin <90247378+kjellbovin@users.noreply.github.com>
2021-09-17 11:01:59 +0200

    Changes in labb1.py

--- labb1.py
+++ labb1.py
@@ -1,11 +1,13 @@
+import math
+
 print('\n ****************************************************************************')
 print(' Här kan du räkna ut volymen för en liksidig kub och en liksidig tetrahedron')
 print(' ****************************************************************************')
 
 var1 = (input("Ange längden på en av figurens sidor (i cm): "))
-var1 = eval(var1.replace(',', '.'))
-kub = round((var1 ** 3), 2)
-tetra = round(((0.5 / 12) * kub), 2)
+var1 = float(var1.replace(',', '.'))
+volumeCube = var1 ** 3
+volumeTetra = var1 ** 3/6*math.sqrt(2)
 
-print('Kubens volym', kub, 'cm3')
-print('Tetraeders volym', tetra, 'cm3')
+print('Kubens volym', volumeCube, 'cm3')
+print('Tetraeders volym', volumeTetra, 'cm3')
